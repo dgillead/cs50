9dc264d8cf90a9f7acd285f109fd68b5871ea92b
Darnell Gillead <dlgillead@gmail.com>
2017-07-05 18:10:24 -0500

    completed logic to draw left side of pyramid

--- pset1/mario.rb
+++ pset1/mario.rb
@@ -1,7 +1,6 @@
 def generate_pyramid
   height = get_height
   pyramid = draw_pyramid(height)
-  puts '-----'
   puts pyramid
 end
 
@@ -9,19 +8,25 @@ def get_height
   height = 24
   until height <= 23 && height > 0
     puts 'Enter the height of they pyramid, must be less than 23 and greater than 0: '
-    return height = gets.chomp.to_i
+    height = gets.chomp.to_i
   end
+  height
 end
 
 def draw_pyramid(height)
   counter = 2
+  space_counter = height - 1
   pyramid_string = ''
   height.times do
+    space_counter.times do
+      pyramid_string += ' '
+    end
     counter.times do
       pyramid_string += '#'
     end
     pyramid_string += "\n"
     counter += 1
+    space_counter -= 1
   end
   pyramid_string
 end
